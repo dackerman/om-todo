f1f8a505fbb68e6b82342589ab173465813f4576
David Ackerman <david.w.ackerman@gmail.com>
2014-03-30 09:19:41 -0700

    Generated hello-world om project

---
+++ project.clj
@@ -0,0 +1,29 @@
+(defproject om-todo "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2197"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [om "0.5.3"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+               :builds [{:id "dev"
+                         :source-paths ["src"]
+                         :compiler {
+                                     :output-to "main.js"
+                                     :output-dir "out"
+                                     :optimizations :none
+                                     :source-map true}}
+                        {:id "release"
+                         :source-paths ["src"]
+                         :compiler {
+                                     :output-to "main.js"
+                                     :optimizations :advanced
+                                     :pretty-print false
+                                     :preamble ["react/react.min.js"]
+                                     :externs ["react/externs/react.js"]}}]})
